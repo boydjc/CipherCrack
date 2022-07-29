15484f3b0b135b4493004986e3c9b4acf24f4418
Joshua Boyd <boyd.jc.github@gmail.com>
2022-07-28 22:35:09 -0400

    added timer

--- CipherCrack/ViewController.swift
+++ CipherCrack/ViewController.swift
@@ -9,9 +9,75 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = Timer()
+    
+    static let MAX_TIME_IN_MILLISECONDS = 3600000
+    
+    var timeInMilliSeconds = MAX_TIME_IN_MILLISECONDS
+    
+    var timerIsRunning = false
+    
+    @IBOutlet weak var timerDisplay: UILabel!
+    
+    
+    @IBAction func timerStart(_ sender: Any) {
+        if(!timerIsRunning) {
+            runTimer()
+            timerIsRunning = true
+        }
+    }
+    
+    @IBAction func timerStop(_ sender: Any) {
+        if(timerIsRunning) {
+            stopTimer()
+            timerIsRunning = false
+        }
+    }
+    
+    @IBAction func timerReset(_ sender: Any) {
+        if(timerIsRunning) {
+            timer.invalidate()
+        }
+        timeInMilliSeconds = ViewController.MAX_TIME_IN_MILLISECONDS
+        timerDisplay.text = formatTimeString(time: TimeInterval(timeInMilliSeconds))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        timerDisplay.text = formatTimeString(time: TimeInterval(timeInMilliSeconds))
+    }
+    
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    func stopTimer() {
+        timer.invalidate()
+    }
+    
+    @objc func updateTimer() {
+        if(timeInMilliSeconds >= 1) {
+            timeInMilliSeconds -= 1
+        }else {
+            stopTimer()
+        }
+        
+        if(timeInMilliSeconds <= 10000) {
+            timerDisplay.textColor = UIColor.red
+        }
+        
+        timerDisplay.text = formatTimeString(time: TimeInterval(timeInMilliSeconds))
+    }
+
+    func formatTimeString(time: TimeInterval) -> String {
+        
+        let milliseconds = Int(time) % 60
+        let seconds = Int(time) / 1000 % 60
+        let minutes = Int(time) / 60000 % 60
+        let hours = Int(time) / 3600000 % 60
+        
+        return String(format: "%02i:%02i:%02i:%02i", hours, minutes, seconds, milliseconds)
     }
 
 
